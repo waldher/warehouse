cd5e7032e2b979852ba5a5719317ce3631c193f9
Benjamin Waldher <lgbr@laserbunny.net>
2011-08-17 03:15:10 -0700

    Revert "small fixes devise add"
    This reverts commit 3021ed2522cadad72028d1fc8f802fece6ea0bbd.

--- db/migrate/20110815143109_add_user_id_to_realtors.rb
+++
@@ -1,9 +0,0 @@
-class AddUserIdToRealtors < ActiveRecord::Migration
-  def self.up
-    add_column :realtors, :user_id, :integer
-  end
-
-  def self.down
-    remove_column :realtors, :user_id
-  end
-end
--- test/unit/helpers/realestates_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class RealestatesHelperTest < ActionView::TestCase
-end
