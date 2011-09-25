bf4662f0cfc84d964e8d913df480da0534cbf284
Ismail Akram <rubyonrails3@gmail.com>
2011-09-25 12:02:13 +0500

    Fixed javascript;; to javscript:;

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", :href => "javascript;;")
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", :href => "javascript:;")
   end
   
   def link_to_add_fields(name, f, association)
