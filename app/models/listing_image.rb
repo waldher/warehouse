2a67915485355850900e00feba7ea92b3ab39bee
Ismail Akram <rubyonrails3@gmail.com>
2011-09-25 15:41:30 +0500

    Fixed thumbnail

--- app/models/listing_image.rb
+++ app/models/listing_image.rb
@@ -1,5 +1,9 @@
 class ListingImage < ActiveRecord::Base
   belongs_to :listing
 
-  has_attached_file :image, :storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml", :path => ":id/:filename"
+  has_attached_file :image, 
+    :styles => { :thumbnail => "300x400>" },
+    :storage => :s3, 
+    :s3_credentials => "#{Rails.root}/config/s3.yml", 
+    :path => ":id/:style/:filename"
 end
