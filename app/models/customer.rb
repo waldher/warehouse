dc63f9821cc0f28af3eaa3a1bd11ae3f85c2cf6b
Benjamin Waldher <lgbr@laserbunny.net>
2011-09-29 14:58:29 -0700

    Fix customer urls

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -87,8 +87,4 @@ class Customer < ActiveRecord::Base
   def self.password_with_salt(password, salt) 
     Digest::SHA2.hexdigest("Put #{salt} on the #{password}")
   end
-
-  def to_param
-    return key
-  end
 end
