fd31041dbb455c48bb8c9a1d7c0a39ab6b422734
Benjamin Waldher <lgbr@laserbunny.net>
2011-10-21 15:47:42 -0700

    Add random title generator

---
+++ lib/listing_title.rb
@@ -0,0 +1,57 @@
+class ListingTitle
+  UPCASE = Proc.new do |str|
+      str.upcase
+    end
+  TITLECASE = Proc.new do |str|
+      str.titlecase
+    end
+  DOWNCASE = Proc.new do |str|
+      str.downcase
+    end
+
+  TRANSFORMS = [UPCASE, TITLECASE, DOWNCASE]
+
+  WITHS = ["with", "w/", "feat", "has", "incl", nil]
+
+  #
+  #Real Estate
+  #
+  INS = ["in", "at", "on", nil]
+
+  LISTING_ADJECTIVES = ["beatiful", "gorgeous", "classy", "ideal", "attractive", "lovely", "marvelous", "radiant", "wonderful", "attractive", "bright", "comfortable"]
+  LOCATION_ADJECTIVES = ["alluring", "bright", "cozy", "quiet", "comfortable", "attractive", "lovely", "exquisite", "impressive", "pretty", "classy", "wonderful", "fun", "convenient", "accessible"]
+  LOCATION_NOUNS = ["locale", "area", "community", "neighborhood", "location"]
+
+  AMENITIES = ["balcony", "pool", "gym", "hot tub", "tennis", "basketball"]
+  
+  def self.generate(params={})
+    o = { :bedrooms => 0,
+          :type => nil,
+          :location => nil,
+          :amenities => ""}.merge(params)
+
+    br = o[:bedrooms] == 0 ? nil : "#{o[:bedrooms]}br"
+    type = (o[:type] or "unit")
+    location = (o[:location] or LOCATION_NOUNS.sample)
+    amenities_array = []
+
+    for potential_amenity in AMENITIES
+      if o[:amenities] =~ /#{potential_amenity}/i
+        amenities_array << potential_amenity
+      end
+    end
+
+    amenities = amenities_array * " "
+
+    transform = TRANSFORMS.sample
+
+    return transform.call([LISTING_ADJECTIVES.sample,
+                      br,
+                      type,
+                      INS.sample,
+                      LOCATION_ADJECTIVES.sample,
+                      location,
+                      amenities.empty? ? nil : WITHS.sample,
+                      amenities.empty? ? nil : amenities].reject{|word| word.nil? }.join(" "))
+  end
+end
