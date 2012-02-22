de3803b56e489d1bb683c74b9b33718decf22e74
Ismail Akram <rubyonrails3@gmail.com>
2012-02-22 12:47:28 +0500

    Added state NaN if manual_enabled? is nil and color set to postedBlue

--- app/models/listing_info.rb
+++ app/models/listing_info.rb
@@ -2,6 +2,8 @@ class ListingInfo < ActiveRecord::Base
   belongs_to :listing
 
   def value
-    read_attribute(:value).force_encoding("UTF-8")
+    if read_attribute(:value).kind_of?(String)
+       read_attribute(:value).force_encoding("UTF-8")
+    end
   end
 end
