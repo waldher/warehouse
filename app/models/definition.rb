a2a907de053e64cd2d4659b0e2d724314f9c9a59
Benjamin Waldher <lgbr@laserbunny.net>
2012-04-06 07:19:01 -0700

    Add words and definitions

---
+++ app/models/definition.rb
@@ -0,0 +1,16 @@
+class Definition < ActiveRecord::Base
+  has_many :synonyms
+
+  def self.word_synonyms(spellings)
+    query = Word.where("ignore = ?", false)
+    if spellings.is_a?(String)
+      query = query.where("spelling = ?", spellings)
+    elsif spellings.is_a?(Array)
+      query = query.where("spelling in (?)", spellings)
+    end
+
+    definition_ids = query.collect(&:definition_id)
+
+    return Word.where("definition_id in (?) and ignore = ?", definition_ids, false).collect(&:spelling)
+  end
+end
