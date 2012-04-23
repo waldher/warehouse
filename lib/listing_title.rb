e1c439e1842ed38276110a5da1db3d44b1a3e8cf
Benjamin Waldher <lgbr@laserbunny.net>
2012-04-23 03:06:11 -0700

    New version of title generator

--- lib/listing_title.rb
+++ lib/listing_title.rb
@@ -1,4 +1,6 @@
 class ListingTitle
+  MAX_TITLE_LENGTH = 70
+
   UPCASE = Proc.new do |str|
       str.upcase
     end
@@ -11,112 +13,197 @@ class ListingTitle
 
   TRANSFORMS = [UPCASE, TITLECASE, DOWNCASE]
 
-  WITHS = ["with", "w/", "has", "features", "feat.", "incl.", "includes"]
+  ADJECTIVES = ["Great",
+                "Beautiful",
+                "Nice",
+                "Awesome",
+                "Flashy",
+                "Amazing",
+                "Charming",
+                "Cute",
+                "Wonderful",
+                "Gorgeous",
+                "Superb",
+                "Excellent",
+                "Good",
+                "Slick",
+                "Divine",
+                "Majestic",
+                "Enjoyable",
+                "Stunning",
+                "Fabulous",
+                "Very Nice",
+                ]
 
-  POST = ["included", "available", "featured"]
+  ADVERTISING_STRINGS = [ "Must see!",
+                          "Too good to be true!",
+                          "Look here!",
+                          "Look no further!",
+                          "Your search is over!",
+                          "Unique!",
+                          "Rare find!",
+                          "Ready To View!",
+                          "Ready To Show!",
+                          "Check It Out!",
+                          "Ready To Move In!",
+                          "Only now!",
+                          "Dont wait!",
+                          "Chance of a lifetime!",
+                          "Pure Joy!",
+                          "Wow!",
+                          "Don't Miss Out!",
+                          "Great Chance!",
+                          "Great Opportunity!",
+                          "Cant Believe Its Still Available!",
+                          "Now Or Never!",
+                          "What An Opportunity!",
+                          "Looking For A Deal?",
+                          ]
 
-  INS = ["in", ","]
+  TIME_LIMITS = [ "Hurry Wont Last!",
+                  "Limited Offer!",
+                  "Now Or Never!",
+                  "Limited Opportunity!",
+                  "Dont Wait!",
+                  "Limited Time Only!",
+                  ]
 
-  LISTING_ADJECTIVES = ["beautiful", "gorgeous", "classy", "attractive", "lovely", "marvelous", "radiant", "wonderful", "comfortable", "excellent", "dazzling", "bewitching"]
-  LOCATION_ADJECTIVES = ["alluring", "cozy", "quiet", "comfortable", "attractive", "lovely", "exquisite", "impressive", "classy", "wonderful", "fun", "convenient", "accessible"]
-  
-  ADJECTIVES = ["accessible", "adorable", "alluring", "amazing", "angelic", "appealing", "ardent", "arousing", "artful", "astonishing", "attractive", "beautiful", "bewitching", "blissful", "breathtaking", "captivating", "charming", "chic", "classic", "classy", "colorful", "comfortable", "convenient", "cozy", "dapper", "dashing", "dazzling", "delightful", "delighting", "desirable", "divine", "dreamy", "ebullient", "effusive", "elating", "elegant", "enchanted", "enchanting", "energetic", "enjoyable", "enticing", "euphoric", "excellent", "exciting", "exhilarating", "expansive", "exquisite", "extravagant", "exuberant", "exulting", "eye-catching", "fabulous", "fancy", "fantastic", "fashionable", "flashy", "foxy", "fresh", "fun", "glamorous", "glorious", "glowing", "gorgeous", "grand", "grandiose", "handsome", "impressive", "intelligent", "intoxicating", "inviting", "jolly", "joyful", "joyous", "jubilant", "keen", "lavish", "lovely", "luscious", "lush", "luxurious", "magnetic", "magnificent", "majestic", "marvelous", "mighty", "modern", "opulent", "penetrating", "peppy", "perceptive", "perky", "phenomenal", "pleasant", "plush", "pointed", "pretty", "profound", "quiet", "radiant", "refined", "remarkable", "ritzy", "sapient", "saucy", "savvy", "scintillating", "seductive", "sensational", "sensible", "sharky", "sharp", "showy", "shrewd", "slick", "smart", "sparkling", "spectacular", "splendid", "sporty", "sprightly", "striking", "stunning", "stupendous", "stylish", "sublime", "sumptuous", "superb", "superior", "swank", "tantalizing", "vibrant", "vivacious", "wonderful", "wondrous"] 
+  AMENITIES = [ /Pool/i,
+                /Gym/i,
+                /Hot Tub/i,
+                /Tennis/i,
+                /Basketball/i,
+                /Large Yard/i,
+                /Parking/i,
+                /Garage/i,
+                /Exercise Room/i,
+                /Tiled/i,
+                /Terrace/i,
+                /Terrace/i,
+                /Sauna/i,
+                /Patio/i,
+                /Winter Garden/i,
+                /Barbeque/i,
+                /Vaulted Ceiling/i,
+                /Furnished/i,
+                /Sun Deck/i,
+                /Walk-In Closet/i,
+                /Walk-In Closets/i,
+                /Art Decor/i,
+                ]
+  #TODO
+  #ame9: [Material] Floors
+  #ame11: [Material] Kitchen
+  #ame22: New [Kitchen, etc.]
+  #ame25: Large [dining, living, etc.] Room
+  #ame26: [Ocean, Waterway, etc.] View
 
-  LOCATION_NOUNS = ["locale", "community", "neighborhood", "location"]
-
-  AMENITIES = ["balcony", "pool", "gym", "hot tub", "tennis", "basketball"]
- 
-  BR_PRE = [";"]
-  BR = ["br","bed"]
-
-  def self.generate(params={})
-    o = { :bedrooms => 0,
-          :type => "",
-          :location => "",
-          :amenities => ""}.merge(params)
-
-    br = (o[:bedrooms] == 0 or o[:bedrooms].nil?) ? "" : "#{o[:bedrooms]}#{BR.sample}"
-    type = (o[:type])
-    location = (o[:location] or LOCATION_NOUNS.sample)
-    amenities_array = []
-
-    for potential_amenity in AMENITIES
-      if o[:amenities] =~ /#{potential_amenity}/
-        amenities_array << (potential_amenity + (["tennis","basketball"].include?(potential_amenity) ? " court" : ""))
-      end
-    end
-
-    amenities = amenities_array.reject{|a| a.nil? or a.empty?}
-    if amenities.size > 1
-      amenities = amenities[0..-2].join(", ") + " and " + amenities[-1]
-    elsif amenities.size == 1
-      amenities = amenities.first
-    else
-      amenities = ""
-    end
-
-    bedrooms  = [
-                  [BR_PRE.sample, "#{ADJECTIVES.sample} #{br} #{type}"],
-                  [BR_PRE.sample, "#{ADJECTIVES.sample} #{type}#{", "+br if !br.empty?}"]
-                ].sample
-
-    location  = [
-                  [INS.sample, "#{ADJECTIVES.sample} #{location}"]
-                ].sample
-
-    amenities = [
-                  [amenities.empty? ? nil : ","                  , amenities.empty? ? nil : "#{amenities} #{POST.sample}"],
-                  [amenities.empty? ? nil : ", apt " + WITHS.sample, amenities.empty? ? nil : amenities]
-                ].sample
-
-    if amenities[0].nil? or amenities[1].nil?
-      title_parts = [bedrooms, location].shuffle
-    else
-      title_parts = [bedrooms, location, amenities].shuffle
-    end
-    #puts "Parts : #{title_parts.to_s}"
-
-    title = [ 
-              title_parts[0][1],
-              title_parts[0][1].nil? ? nil : title_parts[1][0],
-              title_parts[1][1]
+  PERKS = [ /Close To Beach/i,
+            /Gated Community/i,
+            /Live Security/i,
+            /Close To Preserve/i,
+            /Golf Course Close/i,
+            /Near University/i,
+            /Close[ \-]?By Shops/i,
+            /Close To Public Transportation/i,
+            /Close To Everything/i,
+            /In Quiet Area/i,
             ]
-    if !amenities[0].nil? and !amenities[1].nil?
-      title += [
-                (title_parts[0][1].nil? and title_parts[1][1].nil?) ? nil : title_parts[2][0],
-                 title_parts[2][1]
-               ]
-    end
-    
-    #puts "Pre replace : #{title.to_s}"
-    title = title.join(" ").gsub(/[ ]*([ ,;])/,'\1')
 
-    title = TRANSFORMS.sample.call(title)
+  AGES = [/modern/i,
+          /remodelled/i,
+          /refurbished/i,
+          /rustic style/i,
+          /renovated/i,
+          ]
 
-    #puts "Pre truncated : \"#{title}\""
-    while title.length > 70
-      title = title.split(/ /)[0..-2].join(' ')
-    end
-    
-    #all possible options for surrounding characters
-    brackets = "{} [] ** ~~ :: -- ++ == __ ## !! $$ () )(".split(" ") << "  " << "\\/" << "/\\"
-    brackets = brackets.collect{|b| b.split('')}.sample
-    max = rand(11)
-    max = max - max % 2 #give even number
-    index = -1
-    while title.length <= 69
-      index += 1
-      break if index >= max
 
-      #puts "(#{index} of #{max})"
-      (title = title + " "; next) if (index == 0 and title.last  != " ")
-      (title = " " + title; next) if (index == 1 and title.first != " ")
-      
-      if index % 2 == 0
-        title = title + brackets[1]
-      else
-        title = brackets[0] + title
+  BR = ["br","bed","bd"]
+
+  TEMPLATES = [ "[<til>] <rai> <adj> <bdr> [<age>] <top> In [<are>, ]<loc>[, Features <ame>][,<per>]",
+                "<rai> [<age>] <adj> <top> with <bdr>[, <ame>][, <per>] In [<are>, ]<loc>, [<til>]",
+                "<adj> <top>, <bdr>[, <ame>] In [<are>, ]<loc>[, <per>], <rai>, [<age>][<til>]",
+                "<adj> [<age> ] <bdr> <top> in [<are>, ]<loc>[, <per>,] [<til>, ]<rai> [, Features <ame>]",
+                "This [, <age>]<adj> <top> In [<are>, ]<loc>, [Features <ame>][, <per>] <bdr>s[, <til>], <rai>",
+                ]
+
+  def self.generate(listing)
+    bdr = (listing.infos["ad_bedrooms"] == "0" or listing.infos["ad_bedrooms"].nil?) ? "" : "#{listing.infos["ad_bedrooms"]}#{BR.sample}"
+    loc = (listing.infos["ad_location"] or "")
+    top = (listing.infos["ad_type"] or "")
+    adj = ADJECTIVES.sample
+
+    amenities_array = []
+    for potential_amenity in AMENITIES
+      match = potential_amenity.match(listing.infos["ad_amenities"]) or potential_amenity.match(listing.infos["ad_description"])
+      if match
+        amenities_array << match[0]
       end
+    end
+    ame = (amenities_array.sample or "")
+
+    til = TIME_LIMITS.sample
+
+    ages_array = []
+    for potential_age in AGES
+      match = potential_age.match(listing.infos["ad_description"])
+      if match
+        ages_array << match[0]
+      end
+    end
+    age = (ages_array.sample or "")
+
+    perks_array = []
+    for potential_perk in PERKS
+      match = potential_perk.match(listing.infos["ad_description"])
+      if match
+        perks_array << match[0]
+      end
+    end
+    per = (perks_array.sample or "")
+
+    rai = ADVERTISING_STRINGS.sample
+
+    # BEGIN TEMPLATE GENERATION
+    templates_list = TEMPLATES
+    title = nil
+    while title.nil? and !templates_list.empty?
+      template = templates_list.sample
+      prospect = String.new(template)
+
+      prospect.gsub!(/<bdr>/, bdr)
+      prospect.gsub!(/<loc>/, loc)
+      prospect.gsub!(/<top>/, top)
+      prospect.gsub!(/<adj>/, adj)
+      prospect.gsub!(/<ame>/, ame)
+      prospect.gsub!(/<til>/, til)
+      prospect.gsub!(/<age>/, age)
+      prospect.gsub!(/<per>/, per)
+      prospect.gsub!(/<rai>/, rai)
+
+      prospect.gsub!(/  */, ' ') # Remove double spaces
+
+      puts bdr
+      base_title = prospect.gsub(/ *\[[^\[\]]*\] */, ' ')
+
+      if base_title.length > 70
+        templates_list -= [template]
+        next
+      end
+
+      optional_match_list = []
+      for optional_match in prospect.scan(/\[[^\[\]]*\]/)
+        optional_match_list << optional_match[1..-2]
+      end
+      optional_match_list.shuffle
+      
+      for match in optional_match_list
+        if (base_title.size + match.size) <= 70
+          base_title = prospect.gsub(/\[#{match}\]/, match).gsub(/ *\[[^\[\]]*\] */, ' ').gsub(/^ */, '').gsub(/ *$/, '')
+        end
+      end
+
+      title = prospect.gsub(/ *\[[^\[\]]*\] */, ' ').gsub(/^ */, '').gsub(/ *$/, '')
     end
     
     return title
