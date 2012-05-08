37c0d3691be70dcb03e35a3c78f3300e23979c83
Brendan Winter <brendan@leadadvo.com>
2012-05-07 17:53:35 -0700

    Removing some unneeded queries that were originally inteded for testing

--- app/models/word.rb
+++ app/models/word.rb
@@ -19,12 +19,12 @@ class Word < ActiveRecord::Base
       adj_ids << s.wordnet_number if s.category == 'a'
       nou_ids << s.wordnet_number if s.category == 'n'
       sat_ids << s.wordnet_number if s.category == 's'
-      puts ""
-      puts "#{s.symbol} - #{s.wordnet_number} - #{s.category}"
-      puts Definition.where(:wordnet_number => s.wordnet_number, :category => s.category).sample.text_definition
-      for w in Definition.where(:wordnet_number => s.wordnet_number, :category => s.category).sample.words
-        puts w.spelling
-      end
+      #puts ""
+      #puts "#{s.symbol} - #{s.wordnet_number} - #{s.category}"
+      #puts Definition.where(:wordnet_number => s.wordnet_number, :category => s.category).sample.text_definition
+      #for w in Definition.where(:wordnet_number => s.wordnet_number, :category => s.category).sample.words
+      #  puts w.spelling
+      #end
     }
 
     definition_ids = Definition.where("(wordnet_number in (?) and category = 'a') or
