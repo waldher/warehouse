cdd48ea4c3911d77ed23db4139707624fc13cbea
Allam Matsubara <allam.matsubara@gmail.com>
2012-05-24 02:57:37 -0300

    Reverting sublocation model to the original form.

--- app/models/sublocation.rb
+++ app/models/sublocation.rb
@@ -1,11 +1,10 @@
 class Sublocation < ActiveRecord::Base
   belongs_to :location
   has_many :customers
-  validates_presence_of :name
 
-  before_validation :caps
+  after_initialize :caps
 
   def caps
-    self.name.capitalize! if name
+    self.name.capitalize!
   end
 end
