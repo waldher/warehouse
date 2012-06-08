df0a6611c0321cc52bdd5b0cc8e371023c5c5f95
Benjamin Waldher <lgbr@laserbunny.net>
2012-06-07 21:00:52 -0700

    Sort customers page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,7 @@ class AdminController < ApplicationController
   before_filter :authenticate_admin!
 
   def index
-    @customers = Customer.all
+    @customers = Customer.order("key asc")
   end
 
 end
