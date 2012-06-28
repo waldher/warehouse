e5f1ecaa7bbf87136e1dc2da17f0616639eb10af
Brendan Winter <brendan@leadadvo.com>
2012-06-28 16:57:42 -0700

    Making it so location can be nil as well. It shouldn't force a location, if none is provided something is wrong and needs to be delt with.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -53,7 +53,7 @@ class ListingsController < ApplicationController
           :ad_image_urls => (CSV.parse(listing.images_array[1..-2]).first rescue []), 
           :ad_autokeywords => listing.autokeywords,
           :listing_infos => (Hash[CSV.parse(listing.info_keys_array[1..-2]).first.zip CSV.parse(listing.info_values_array[1..-2].gsub("\\\"", "\"\"")).first] rescue {}), 
-          :location => ((listing.location and listing.location.url) or (listing.customer.location and listing.customer.location.url) or "miami"), 
+          :location => ((listing.location and listing.location.url) or (listing.customer.location and listing.customer.location.url) or nil), 
           :sublocation => ((listing.sublocation and listing.sublocation.url) or (listing.customer.sublocation and listing.customer.sublocation.url) or nil), 
           :ad_foreign_id => listing.foreign_id
         }
